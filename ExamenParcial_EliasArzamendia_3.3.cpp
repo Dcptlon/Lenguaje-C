159003ca1b37d6030e434c729b94d0fceaef6537
Dcptlon <eliasarzamendia015@gmail.com>
2024-10-08 01:18:22 -0300

    Actualizar ExamenParcial_EliasArzamendia_3.3.cpp

--- ExamenParcial_EliasArzamendia_3.3.cpp
+++ ExamenParcial_EliasArzamendia_3.3.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <vector>
 /*--------------------------------ELIAS ARZAMENDIA-----------------------------------*/
 using namespace std;
 int main() {
-    int matriz[3][3] = {
+    vector<vector<int> > matriz = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
     cout << "Matriz 3x3:" << endl;
-    for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+    for(int i = 0; i < matriz.size(); i++){
+            for(int j = 0; j < matriz[i].size(); j++){
                 cout << matriz[i][j] << " ";
             }
         cout << endl;
@@ -17,7 +18,7 @@ int main() {
     cout << endl;
 
     cout << "Diagonal principal de la matriz 3x3:" << endl;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < matriz.size(); i++) {
         cout << matriz[i][i] << " ";
     }
     cout << endl;
